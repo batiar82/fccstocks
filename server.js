a5cf30060ed61bb1eea39451a01acf3c4a8a2f46
Mariano <batiar@gmail.com>
2018-03-29 09:29:29 -0300

    Mas para q ande en heroku

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const ws = require('./ws');
 const app = express();
+const path = require('path');
 const port = process.env.PORT || 5000;
-
-const routes=require('./routes/index.js');
+app.use(express.static(path.join(__dirname,'client/build')));
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express from express' });
 });
